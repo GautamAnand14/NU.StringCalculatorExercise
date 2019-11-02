92184cc6e3c89cc6236eb635a86bcd63747932b4
gautamanand14 <gautambhudiraja@gmail.com>
2019-11-02 20:16:42 +0000

    Project Template (Architect) setup for initial development phase

---
+++ NU.StringCalculatorExercise/NU.StringCalculatorExerciseTest/StringCalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NU.StringCalculatorExercise;
+
+namespace NU.StringCalculatorExerciseTest
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        private StringCalculator stringCalculator;
+
+        [TestInitialize]
+        public void StringCalculatorTestSetup()
+        {
+            stringCalculator = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void StringCalculator1()
+        {
+        }
+    }
+}
